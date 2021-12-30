aa2e1343a44fa6fd060c55823d1f6cac11641e22
yazid113 <92461753+yazid113@users.noreply.github.com>
2021-12-29 22:54:49 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,36 +4,42 @@ function fetchData(url){
     fetch(url)
     .then(res => res.json())
     .then(data => data.results.map(profile => {
-        console.log(profile)
-        profile=[profile.name.first,profile.name.last,profile.email,profile.picture.medium]
-        console.log(profile)
+        console.log
+       const email= profile.email
+       const fullName= `${profile.name.first} ${profile.name.last}`
+       const picture= profile.picture.thumbnail
+       const location= `${profile.location.city},${profile.location.state}`
+        getHTML(email,picture,fullName,location)
     }))
+
         
         
-        // const emails = data.results.map(email => email.email)
-        // console.log(emails)
-        // const photo = data.results.map(img => img.picture.medium)
-        // console.log(photo)
-        // const fullName = data.results.map(fullName => `${fullName.name.first} ${fullName.name.last}`)
-        // console.log(fullName)
-        
-    
 }
-function getHTML(emails,photo,fullName) {
+function getHTML(email,picture,fullName,location) {
     const html = `
     <div class="card">
                     <div class="card-img-container">
-                        <img class="card-img" src="${photo}" alt="profile picture">
+                        <img class="card-img" src=${picture} alt="profile picture">
                     </div>
                     <div class="card-info-container">
                         <h3 id="name" class="card-name cap">${fullName}</h3>
-                        <p class="card-text">${emails}</p>
-                        <p class="card-text cap">city, state</p>
+                        <p class="card-text">${email}</p>
+                        <p class="card-text cap">${location}</p>
                     </div>
                 </div>
                 `;
-    gallery.innerHTML = html;
+    gallery.insertAdjacentHTML('afterbegin',html)
     
+}
+
+function popUP(){
+    const card = document.querySelectorAll('#gallery div')
+    card.addEventListener('click',function click(e) {
+        if(e.target.className = 'card'){
+         console.log('hello')   
+        }
+        
+    })
 }
 fetchData('https://randomuser.me/api/?results=12')
 
