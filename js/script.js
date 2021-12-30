7e5071532461de75690894a09696d13d80f175e9
yazid113 <92461753+yazid113@users.noreply.github.com>
2021-12-29 19:54:59 -0500

    Update

---
+++ js/script.js
@@ -0,0 +1,38 @@
+const gallery = document.querySelector('#gallery')
+
+function fetchData(url){
+    fetch(url)
+    .then(res => res.json())
+    .then(data => {
+        console.log(data.results)
+        const emails = data.results.map(email => email.email)
+        console.log(emails)
+        const photo = data.results.map(img => img.picture.medium)
+        console.log(photo)
+        const fullName = data.results.map(fullName => `${fullName.name.first} ${fullName.name.last}`)
+        console.log(fullName)
+        .then(profile => {
+            profile.getHTML(emails,photo,fullName)
+        })
+        
+    })
+    
+}
+function getHTML(emails,photo,fullName) {
+    const html = `
+    <div class="card">
+                    <div class="card-img-container">
+                        <img class="card-img" src="${photo}" alt="profile picture">
+                    </div>
+                    <div class="card-info-container">
+                        <h3 id="name" class="card-name cap">${fullName}</h3>
+                        <p class="card-text">${emails}</p>
+                        <p class="card-text cap">city, state</p>
+                    </div>
+                </div>
+                `;
+    gallery.innerHTML = html;
+    
+}
+fetchData('https://randomuser.me/api/?results=12')
+
