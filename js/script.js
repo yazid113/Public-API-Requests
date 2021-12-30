c4ea57ccd068dfbd7b080729474d368a22477c62
yazid113 <92461753+yazid113@users.noreply.github.com>
2021-12-29 20:34:39 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,19 +3,20 @@ const gallery = document.querySelector('#gallery')
 function fetchData(url){
     fetch(url)
     .then(res => res.json())
-    .then(data => {
-        console.log(data.results)
-        const emails = data.results.map(email => email.email)
-        console.log(emails)
-        const photo = data.results.map(img => img.picture.medium)
-        console.log(photo)
-        const fullName = data.results.map(fullName => `${fullName.name.first} ${fullName.name.last}`)
-        console.log(fullName)
-        .then(profile => {
-            profile.getHTML(emails,photo,fullName)
-        })
+    .then(data => data.results.map(profile => {
+        console.log(profile)
+        profile=[profile.name.first,profile.name.last,profile.email,profile.picture.medium]
+        console.log(profile)
+    }))
         
-    })
+        
+        // const emails = data.results.map(email => email.email)
+        // console.log(emails)
+        // const photo = data.results.map(img => img.picture.medium)
+        // console.log(photo)
+        // const fullName = data.results.map(fullName => `${fullName.name.first} ${fullName.name.last}`)
+        // console.log(fullName)
+        
     
 }
 function getHTML(emails,photo,fullName) {
